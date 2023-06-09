7b6f07b2471553ae7bfb4862d284c5284dedbfc9
Jan Herbst <jan.herbst@me.com>
2023-06-09 19:00:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,9 +13,6 @@ const canvasElement = document.getElementById("qr-canvas");
 const canvas = canvasElement.getContext("2d");
 const overlay = document.getElementById("canvas-overlay");
 
-const audioVerification = new Audio("verification.mp3");
-const audioApproval = new Audio("approval.mp3");
-const audioAction = new Audio("action.mp3");
 const audioWait = new Audio("wait.mp3");
 
 var scanning=false;
@@ -103,13 +100,13 @@ qrcode.callback = (res) => {
         console.log(res)
         if(res == "GOTOVERIFIED"){
             moveScene(Images.VERIFIED);
-            audioVerification.play();
+
         }else if (res == "GOTOOVERWRITTEN"){
             moveScene(Images.OVERWRITTEN);
-            audioApproval.play();
+
         }else if (res == "GOTOMENU"){
             moveScene(Images.MENU);
-            audioAction.play();
+
         }
         stopQR();
     }
